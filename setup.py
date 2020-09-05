162a71e593b93f72dcc2d74a5da7436c8ce657cf
valadaa48 <valadaa48@gmx.com>
2020-09-05 16:14:21 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="retrolauncher2",
+    version="0.5.0",
+    description="RetroLauncher 2",
+    author="valadaa48",
+    author_email="valadaa48@gmx.com",
+    url="https://github.com/valadaa48/retrolauncher2",
+    install_requires=["urwid", "toml", "evdev", "cachetools", "humanize"],
+    entry_points={
+        "console_scripts": ["retrolauncher2 = retrolauncher.retrolauncher:main"]
+    },
+)
